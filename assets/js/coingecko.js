0283fbbf6910e88576b1eb1ea40b5f842a63e4ec
ElusiveEntity <AnElusiveEntity@gmail.com>
2021-07-04 12:32:57 -0400

    Added basic directory structuring and empty HTML/JS files.

---
+++ assets/js/coingecko.js
@@ -0,0 +1,17 @@
+
+
+
+//class for easy handling of coinGecko API
+//not all API calls are supported.
+
+class coinGeckoAPI {
+
+    constructor() {
+        //code to be performed on object creation
+
+        this.baseURL = "api.coingecko.com/api/v3";
+    }
+
+
+
+}

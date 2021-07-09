528a4a5266a58fa659581d11a9bd824af5a7a409
ElusiveEntity <AnElusiveEntity@gmail.com>
2021-07-08 21:14:25 -0400

    Created class coinGeckoAPI, with private handling of the /coins/list API call.

--- assets/js/coingecko.js
+++ assets/js/coingecko.js
@@ -3,37 +3,44 @@
 
 class coinGeckoAPI {
 
+    //private vars
+    #baseURL;
+    #coinGeckoSimpleList;
+
     constructor() {
         //code to be performed on object creation
+        //The pound symbol (#) is used in JS to denote a private variable/method.
 
-        this.baseURL = "https://api.coingecko.com/api/v3";
-    }
-
-    performCall() {
+        this.#baseURL = "https://api.coingecko.com/api/v3";
+        this.#coinGeckoSimpleList = this.#listOfAllCoins();
 
     }
     
+    //#region privateMethods
+
     //returns an array of coin objects
-    coinList() {
-        fetch(this.baseURL + "/coins/list")
+    #listOfAllCoins() {
+
+        fetch(this.#baseURL + "/coins/list")
         .then( function(response) {
-            console.log(response);
+            return response.json();
+        })
+        .then( function(data) {
+            return data;
         })
     }
 
+    //#endregion privateMethods
 
+    //#region publicMethods
 
+    getListOfAllCoins() {
+        return this.#coinGeckoSimpleList;
+    }
+
+    //#endregion publicMethods
 }
 
 
 
 var coinGeckoAPI_Instance = new coinGeckoAPI();
-coinGeckoAPI_Instance.coinList();
-
-/*
-class cryptoCurrency {
-    constructor() {
-        
-    }
-}
-*/

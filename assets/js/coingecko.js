1a35f487af1c79503bd3532b64afb76b5316653a
ElusiveEntity <30778577+ElusiveEntity@users.noreply.github.com>
2021-07-10 11:49:21 -0400

    Added instructions on how to use the API interface at the bottom of coingecko.js

--- assets/js/coingecko.js
+++ assets/js/coingecko.js
@@ -34,12 +34,12 @@ class coinGeckoAPI {
 
     //get the full data on a coin from the API.
     //all params other than the first one are optional. You may omit them if you don't need them.
-    async getCoinDataFULL(coinName, localization = false, tickers = false, marketData = false) {
+    async getCoinDataFULL(coinName, localization = false, tickers = false, marketData = true) {
         
         if (typeof localization != "boolean" ||
             typeof tickers != "boolean" ||
             typeof marketData != "boolean") {
-                throw 'Parameter is not a boolean!';
+                throw 'getCoinDataFULL Error: Parameter is not a boolean!';
             }
 
         let apiResponse = await fetch(this.#baseURL + "/coins/" + coinName + "?localization=" + localization +
@@ -61,6 +61,13 @@ class coinGeckoAPI {
 }
 
 
-
+//HOW TO USE:
 var coinGeckoAPI_Instance = new coinGeckoAPI();
-var bitcoinData = coinGeckoAPI_Instance.getCoinDataFULL("bitcoin").then( value => { console.log(value); });
+coinGeckoAPI_Instance.getListOfAllCoins().then( data => {
+    //create a function here to store the returned data, or
+    //call a new function: 
+    //dataHandler(data) { var listOfAllCoins = data; }
+    //note that data => { } is the same thing as function(data){ } that we used in the homework.
+    console.log(data);
+});
+coinGeckoAPI_Instance.getCoinDataFULL("bitcoin").then( data => { console.log(data); });

8762c51010e0d3ee4f1a9e0c3e23953a047df5bb
ElusiveEntity <30778577+ElusiveEntity@users.noreply.github.com>
2021-07-10 11:16:08 -0400

    Added call for full data return of a single coin. Must be handled with .then()

--- assets/js/coingecko.js
+++ assets/js/coingecko.js
@@ -1,3 +1,5 @@
+
+
 //class for easy handling of coinGecko API
 //not all API calls are supported.
 
@@ -19,21 +21,38 @@ class coinGeckoAPI {
     //#region privateMethods
 
     //returns an array of coin objects
-    #listOfAllCoins() {
+    async #listOfAllCoins() {
 
-        fetch(this.#baseURL + "/coins/list")
-        .then( function(response) {
-            return response.json();
-        })
-        .then( function(data) {
-            return data;
-        })
+        let apiResponse = await fetch(this.#baseURL + "/coins/list")
+        let apiData = await apiResponse.json();
+        return apiData;
     }
 
     //#endregion privateMethods
 
     //#region publicMethods
 
+    //get the full data on a coin from the API.
+    //all params other than the first one are optional. You may omit them if you don't need them.
+    async getCoinDataFULL(coinName, localization = false, tickers = false, marketData = false) {
+        
+        if (typeof localization != "boolean" ||
+            typeof tickers != "boolean" ||
+            typeof marketData != "boolean") {
+                throw 'Parameter is not a boolean!';
+            }
+
+        let apiResponse = await fetch(this.#baseURL + "/coins/" + coinName + "?localization=" + localization +
+            "&tickers=" + tickers + "&market_data=" + marketData + 
+            "&community_data=true&developer_data=true&sparkline=true"
+            )
+
+        let apiData = await apiResponse.json();
+        return apiData;
+    }
+    
+    //get the list of all coins.
+    //this is returned as an array of objects, formatted as:
     getListOfAllCoins() {
         return this.#coinGeckoSimpleList;
     }
@@ -44,3 +63,4 @@ class coinGeckoAPI {
 
 
 var coinGeckoAPI_Instance = new coinGeckoAPI();
+var bitcoinData = coinGeckoAPI_Instance.getCoinDataFULL("bitcoin").then( value => { console.log(value); });

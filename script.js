7bcada50196e3ca840f7d910feca9eebeda7de79
AM-Cowles <84042091+AM-Cowles@users.noreply.github.com>
2021-07-14 01:14:27 -0400

    worked script.js

--- script.js
+++ script.js
@@ -2,20 +2,24 @@ var coinGeckoAPI_Instance = new coinGeckoAPI();
 coinGeckoAPI_Instance.get100coinsByMarketCapRank(1).then (data => {
     //create a function here to store the returned data,
     //or
-    //call a new function: 
+    //call a new function:
+<table id="table">
+    <tbody>
+    </tbody>
+</table>
+
 $.ajax({    
     type: "POST",
     url: "/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=",
     data: "IDS="+requests,
     dataType: "json", 
-    success: function(response){
-        var name = response;
+success: function(response){
+    var name = response;
     //Important code starts here to populate table  
-        var yourTableHTML = "";
-        $("table tr:first").after(newRows);
-            jQuery.each(name, function(i,data) {
-                $("#table").append("<tr><td>" + data + "</td></tr>");
-            });
+    var yourTableHTML = "";
+        jQuery.each(name, function(i,data) {
+            $("#table").append("<tr><td>" + data + "</td></tr>");
+        });
 }
 });
     //dataHandler(data) { var listOfAllCoins = data; }

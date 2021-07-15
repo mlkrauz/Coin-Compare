5584412e2f39606e382731c9442e19755992dbfa
ElusiveEntity <AnElusiveEntity@gmail.com>
2021-07-15 16:40:23 -0400

    Added a simple call for coinranking.

---
+++ assets/js/coinranking.js
@@ -0,0 +1,32 @@
+
+
+class coinRankingAPI {
+
+    #baseURL;
+    #key;
+
+    constructor() {
+        this.#baseURL = "https://api.coinranking.com/v2";
+        this.#key = "coinranking4aa5d6e1d6af3eae74fe26c693f35a6ae47613d5b6c5c812";
+    }
+
+    async getSearchSuggestions(searchText) {
+
+        let apiCall = this.#baseURL + "/search-suggestions?query=" + searchText;
+        let apiResponse = await fetch(apiCall, {
+            headers: {
+                'x-access-token': this.#key
+            }
+        });
+
+        let apiData = await apiResponse.json();
+
+        return apiData;
+    }
+
+}
+
+
+//test
+coinRankingInstance = new coinRankingAPI();
+console.log(coinRankingInstance.getSearchSuggestions("bit"));
